2b5f093c2d5bddb25e8cea06b1e7d63fa3779035
Hubert Jasudowicz <hubert.jasudowicz@gmail.com>
2017-02-14 13:40:34 +0100

    Add installation of zplug

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 DOTDIR=$HOME/.dotfiles
 
 echo "Linking .symlink files to ${HOME}..."
-echo "=============================="
+echo "========================================"
 
 files=$(find -H "$DOTDIR" -name '*.symlink')
 for file in $files; do
@@ -17,11 +17,20 @@ for file in $files; do
 done
 
 echo "Installing zgen..."
-echo "=============================="
+echo "========================================"
 if [ ! -d $HOME/.zgen ]; then
     git clone https://github.com/tarjoilija/zgen.git "${HOME}/.zgen"
 else
     echo "${HOME}/.zgen exists. Ignoring."
+fi
+
+echo "Installing vimplug for Neovim..."
+echo "========================================"
+if [ -e $HOME/.local/share/nvim/site/autoload/plug.vim ]; then
+    echo "vim-plug already installed. Ignoring."
+else
+    curl -fLo $HOME/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi
 
 
